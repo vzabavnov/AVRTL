6327de446b49f216fe685dbeb2e14d92ef378610
Vadim Zabavnov <vadim@zabavnov.com>
2017-04-13 12:17:04 -0400

    removed Pin and Port classes overridden by DigitalPort and DigitalPin

--- Include/atlstd.h
+++ Include/atlstd.h
@@ -135,25 +135,6 @@ typedef DigitalPort<DataPortD, DataDirectionPortD> DigitalPortD;
 #endif // PORTD
 
 
-
-// standard ports
-
-#ifdef PORTA
-constexpr Port PortA = Port(_SFR_IO_ADDR(PORTA), _SFR_IO_ADDR(DDRA), _SFR_IO_ADDR(PINA));
-#endif
-
-#ifdef PORTB
-constexpr Port PortB = Port(_SFR_IO_ADDR(PORTB), _SFR_IO_ADDR(DDRB), _SFR_IO_ADDR(PINB));
-#endif
-
-#ifdef PORTC
-constexpr Port PortC = Port(_SFR_IO_ADDR(PORTC), _SFR_IO_ADDR(DDRC), _SFR_IO_ADDR(PINC));
-#endif
-
-#ifdef PORTD
-constexpr Port PortD = Port(_SFR_IO_ADDR(PORTD), _SFR_IO_ADDR(DDRD), _SFR_IO_ADDR(PIND));
-#endif
-
 }
 }
 
