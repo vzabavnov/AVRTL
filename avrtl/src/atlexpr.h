570c198d05a8cebd8aa03c1150d2e10bb16f469a
Vadim Zabavnov <vadim@zabavnov.com>
2017-04-14 13:37:39 -0400

    Added template method to create bit mask for multiple bits; added method to convert value from bits to a number and convert numbers to bits

--- avrtl/src/atlexpr.h
+++ avrtl/src/atlexpr.h
@@ -18,25 +18,56 @@ namespace expr {
 /// <summary>
 /// create mask for a single bit specified
 /// </summary>
-template<typename T = uint8_t>
-constexpr T CreateBitMask(const int bit) {
-	return ((T)1) << bit;
+template<typename T = uint8_t, int bit>
+constexpr T CreateBitMask() {
+	return  bit < 0 ? 0 : ((T)1) << bit;
 }
 
 /// <summary>
-/// Generate a mask with specified length
-/// </summary
+/// create mask for specified bits
+/// </summary>
+template<typename T = uint8_t, int bit1, int bit2, int... pins>
+constexpr T CreateBitMask() {
+	return CreateBitMask<T, bit1>() | CreateBitMask<T, bit2, pins...>();
+}
+
 template<typename T = uint8_t>
-constexpr T CreateMask(const int length) {
-	return length <= 0 ? (T)0 : CreateBitMask<T>(length -1 ) | CreateMask<T>(length - 1);
+constexpr T CreateMask(int length, int offset = 0) {
+	return length <= 0 	? T(0) : CreateMask<T>(length - 1, offset);
+}
+
+template<typename T>
+constexpr inline T ValueFromPins(T const value) {
+	return  T(0);
 }
 
 /// <summary>
-/// Generate a mask with specified length
-/// </summary
-template<typename T = uint8_t>
-constexpr T CreateMask(const int length, const int offset) {
-	return CreateMask(length) << offset;
+/// convert value from specified pins to a number
+/// </summary>
+template<typename T, int pin, int... pins>
+constexpr inline T ValueFromPins(T const value) {
+	const T masked = value & CreateBitMask<T, pin>();
+	return ((pin > sizeof...(pins))
+	? masked >> (pin - sizeof...(pins))
+	: masked << (sizeof...(pins) - pin))
+	| ValueFromPins<T, pins...>(value);
+}
+
+template<typename T>
+constexpr inline T ValueToPins(T const value) {
+	return  T(0);
+}
+
+/// <summary>
+/// convert specified value to specified pins
+/// </summary>
+template<typename T, int pin, int... pins>
+constexpr inline T ValueToPins(T const value) {
+	const T masked = value & CreateBitMask<T, sizeof...(pins)>();
+	return ((pin > sizeof...(pins))
+	? masked << (pin - sizeof...(pins))
+	: masked >> (sizeof...(pins) - pin)
+	) | ValueToPins<T, pins...>(value);
 }
 
 }
